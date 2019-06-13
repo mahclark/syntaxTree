5306195663f846074a5371c445507ac15df6d805
mahclark <mc2124@cam.ac.uk>
2019-06-13 19:43:57 +0100

    made methods class methods

--- demo.py
+++ demo.py
@@ -1,13 +1,17 @@
+from random import randint
 import syntaxTree
 
 tree = syntaxTree.Tree()
 tree.setRoot("Root", True, True)
-tree.addNode("A", True, False)
-tree.addNode("B", False, False)
-tree.addNode("C", False, False)
+# tree.addNode("A", True, False)
+# tree.addNode("B", False, False)
+# tree.addNode("C", False, False)
 
 tree.draw()
 
 done = False
 while not done:
-	done = syntaxTree.loop(tree)
+	done = tree.loop()
+	if not tree.getComplete():
+		tree.addNode("?", randint(0,1), randint(0,1))
+		tree.draw()
--- syntaxTree.py
+++ syntaxTree.py
@@ -3,11 +3,6 @@ import time
 from math import sqrt
 from random import randint
 
-xSize, ySize = 600, 450
-screen = pygame.display.set_mode((xSize, ySize), pygame.RESIZABLE)
-pygame.display.set_caption("Tree")
-pygame.init()
-
 class Node():
 
     def __init__(self, parent, key, hasLeft, hasRight):
@@ -31,38 +26,12 @@ class Node():
     x = 0
     y = 0
 
-class Tree():
-    nodes = []
-    complete = False
-    root = None
-
-    # def __init__(self):
-    #     pass
-
-    def setRoot(self, key, left, right):
-        self.root = Node(None, key, left, right)
-        return self.root
-
-    def addNode(self, key, left, right):
-        nextNode = getNext(self.root)
-        node = Node(nextNode, key, left, right)
-        self.complete = getNext(self.root) == None
-        self.nodes.append(node)
-        return node
-
-    def draw(self):
-        setPositions(self.root)
-        screen.fill([255,255,255])
-        drawTree(self.root)
-        pygame.display.flip()
-
-
-def getNext(node):
+def _getNext(node):
     if node.hasRight:
         if node.right == None:
             return node
 
-        nextNode = getNext(node.right)
+        nextNode = _getNext(node.right)
         if nextNode != None:
             return nextNode
         #else right be full
@@ -71,119 +40,134 @@ def getNext(node):
         if node.left == None:
             return node
 
-        nextNode = getNext(node.left)
+        nextNode = _getNext(node.left)
         if nextNode != None:
             return nextNode
         #else both be full
-    
+
     return None
 
-# def getLevel(n):
-#     if n == 0: return [root]
-
-#     level = []
-#     for parent in getLevel(n - 1):
-#         if parent.left != None:
-#             level.append(parent.left)
-#         if parent.right != None:
-#             level.append(parent.right)
-
-#     return level
-
-leaves = []
-def getDepth(node):
-    leftDepth, rightDepth = 0, 0
-    if node.left != None:
-        leftDepth = 1 + getDepth(node.left)
-    if node.right != None:
-        rightDepth = 1 + getDepth(node.right)
-
-    if node.left == None and node.right == None:
-        leaves.append(node)
-
-    return max(leftDepth, rightDepth)
-depth = 0
-
-def getY(level):
-    return 20 if depth == 0 else int((ySize - 40)*level/depth + 20)
-
-def setPos(node, level):
-    node.y = getY(level)
-    if node.left != None and node.right != None:
-        x = int((setPos(node.left, level + 1) + setPos(node.right, level + 1))/2)
-    elif node.right != None:
-        x = setPos(node.right, level + 1)
-    elif node.left != None:
-        x = setPos(node.left, level + 1)
-    else:
-        x = node.x
-
-    node.x = x
-    return x
-
-def setPositions(root):
-    global leaves, depth
+class Tree():
+    nodes = []
+    complete = False
+    root = None
     leaves = []
-    depth = getDepth(root)
+    depth = 0
 
-    for (n, leaf) in enumerate(leaves):
-        leaf.x = int((n + 0.5)*xSize/len(leaves))
+    xSize, ySize = 600, 450
+    screen = None
+    clock = pygame.time.Clock()
+    frameCount = 0
 
-    setPos(root, 0)
+    def __init__(self):
+        self.screen = pygame.display.set_mode((self.xSize, self.ySize), pygame.RESIZABLE)
+        pygame.display.set_caption("Tree")
+        pygame.init()
 
-def drawTree(node):
-    if node.parent != None:
-        pygame.draw.line(screen, [0,0,0], (node.x, node.y), (node.parent.x, node.parent.y), 2)
+    def setRoot(self, key, left, right):
+        self.root = Node(None, key, left, right)
+        return self.root
 
-    myfont = pygame.font.SysFont("monospace", 15)
-    label = myfont.render(str(node.key), 1, (0,0,0))
-    screen.blit(label, (node.x + 10, node.y - 9))
+    def addNode(self, key, left, right):
+        nextNode = _getNext(self.root)
+        node = Node(nextNode, key, left, right)
+        self.complete = _getNext(self.root) == None
+        self.nodes.append(node)
+        return node
 
-    if node.left != None:
-        drawTree(node.left)
-    if node.right != None:
-        drawTree(node.right)
-    pygame.draw.circle(screen, [20,50,150], (node.x, node.y), 7)
+    def draw(self):
+        self._setPositions(self.root)
+        self.screen.fill([255,255,255])
+        self._drawTree(self.root)
+        pygame.display.flip()
 
-    if node.hasLeft == False:
-        label = myfont.render("x", 1, (0,0,0))
-        screen.blit(label, (node.x - 15, node.y + 2))
+    def getComplete(self):
+        return self.complete
 
-    if node.hasRight == False:
-        label = myfont.render("x", 1, (0,0,0))
-        screen.blit(label, (node.x + 10, node.y + 2))
+    def _setPos(self, node, level):
+        node.y = self._getY(level)
+        if node.left != None and node.right != None:
+            x = int((self._setPos(node.left, level + 1) + self._setPos(node.right, level + 1))/2)
+        elif node.right != None:
+            x = self._setPos(node.right, level + 1)
+        elif node.left != None:
+            x = self._setPos(node.left, level + 1)
+        else:
+            x = node.x
 
-clock = pygame.time.Clock()
-frameCount = 0
-mouseHold = False
-done = False
+        node.x = x
+        return x
 
-def loop(tree):
-    global xSize, ySize
-    # frameCount += 1
-    done = False
-    mx, my = pygame.mouse.get_pos()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.VIDEORESIZE:
-            screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
-            xSize, ySize = event.dict['size']
-            print(xSize)
-            tree.draw()
+    def _setPositions(self, root):
+        self.leaves = []
+        self.depth = self._getDepth(root)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouseHold = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            mouseHold = False
+        for (n, leaf) in enumerate(self.leaves):
+            leaf.x = int((n + 0.5)*self.xSize/len(self.leaves))
 
-    pygame.display.flip()
-    clock.tick(60)
+        self._setPos(root, 0)
 
-    return done
+    def _getDepth(self, node):
+        leftDepth, rightDepth = 0, 0
+        if node.left != None:
+            leftDepth = 1 + self._getDepth(node.left)
+        if node.right != None:
+            rightDepth = 1 + self._getDepth(node.right)
 
-def quit():
-    pygame.quit()
+        if node.left == None and node.right == None:
+            self.leaves.append(node)
+
+        return max(leftDepth, rightDepth)
+
+    def _getY(self, level):
+        return 20 if self.depth == 0 else int((self.ySize - 40)*level/self.depth + 20)
+
+    def _drawTree(self, node):
+        if node.parent != None:
+            pygame.draw.line(self.screen, [0,0,0], (node.x, node.y), (node.parent.x, node.parent.y), 2)
+
+        myfont = pygame.font.SysFont("monospace", 15)
+        label = myfont.render(str(node.key), 1, (0,0,0))
+        self.screen.blit(label, (node.x + 10, node.y - 9))
+
+        if node.left != None:
+            self._drawTree(node.left)
+        if node.right != None:
+            self._drawTree(node.right)
+        pygame.draw.circle(self.screen, [20,50,150], (node.x, node.y), 7)
+
+        if node.hasLeft == False:
+            label = myfont.render("x", 1, (0,0,0))
+            self.screen.blit(label, (node.x - 15, node.y + 2))
+
+        if node.hasRight == False:
+            label = myfont.render("x", 1, (0,0,0))
+            self.screen.blit(label, (node.x + 10, node.y + 2))
+
+    def loop(self):
+        self.frameCount += 1
+        done = False
+        mx, my = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.VIDEORESIZE:
+                screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
+                self.xSize, self.ySize = event.dict['size']
+                self.draw()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseHold = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouseHold = False
+
+        pygame.display.flip()
+        self.clock.tick(60)
+
+        return done
+
+    def quit(self):
+        pygame.quit()
 
 
 if __name__ == "__main__":
@@ -215,19 +199,6 @@ if __name__ == "__main__":
                 mouseHold = True
             if event.type == pygame.MOUSEBUTTONUP:
                 mouseHold = False
-
-                # for node in nodes:
-                #     sqx = (mx - node.x)**2
-                #     sqy = (my - node.y)**2
-
-                #     if sqrt(sqx + sqy) < 7:
-                #         child = Node(node, "?", (node.left == None))
-
-                # draw()
-
-        # if not complete and frameCount % 1 == 0:
-        #     addNode("", randint(0,1), randint(0,1))
-        #     draw()
 
         pygame.display.flip()
         clock.tick(60)
